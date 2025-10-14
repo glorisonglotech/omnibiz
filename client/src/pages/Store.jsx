0278b5afbea97e791af4a67acca28855d98a7ea8
devTechs001 <devtechs842@gmail.com>
2025-10-15 00:52:32 +0300

    added store impl file

---
+++ client/src/pages/Store.jsx
@@ -0,0 +1,10 @@
+const Store = () => {
+    return(
+        <>
+            //all products store for client access linked to view store button in e-commerce,
+            ,all products displayed here
+        </>
+    )
+}
+
+export default Store;
